2c0975c252301da4215e7bde6b38ee4b65ef44a4
Rémi Bossé <remi.bosse@desjardins.com>
2019-03-19 16:28:24 -0400

    fix java

--- .profile.d/javaenv.sh
+++ .profile.d/javaenv.sh
@@ -1,5 +1,6 @@
 echo -----> Setting Java in Path 
-export PATH=$PATH:/home/vcap/app/jre1.8.0_91/bin:/app
-export JAVA_HOME=/home/vcap/app/jre1.8.0_91
+#export PATH=$PATH:/home/vcap/app/jre1.8.0_91/bin:/app
+#export JAVA_HOME=/home/vcap/app/jre1.8.0_91
+export PATH=$PATH:/home/vcap/app/jre/bin
 
 echo -----> set path
