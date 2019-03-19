cfadcb4024e1c01f72ec9cab1bef3734ae000cdb
Rémi Bossé <remi.bosse@desjardins.com>
2019-03-19 16:03:33 -0400

    more info in boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,5 +1,13 @@
 echo "The Mini Java Buildpack is executing! "
 export PATH=$PATH:/home/vcap/apps/jre/bin
 
+
 echo "-----> All files from . = "
 find .
+
+
+echo "-----> PWD = "
+pwd
+
+echo "-----> java -version = "
+java -version
